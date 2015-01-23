07773f2213b4402e609d09374d7a617794e40b7a
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-01-23 11:50:28 -0800

    video 5

--- js/resources.js
+++ js/resources.js
@@ -4,8 +4,11 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-      {name: "background-tiles.png", type:"image", src: "data/img/background-tiles.png"},
-      //{name: "meta-tiles.png", type:"image", src: "data/img/meta-tiles.png"},
+      {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+      {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+      {name: "player", type:"image", src: "data/img/orcSpear.png"},
+
+
 
 	/* Atlases 
 	 * @example
@@ -17,7 +20,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-    //{name: "test", type: "tmx", src: "data/map/test.tmx"}
+    {name: "test", type: "tmx", src: "data/map/test.tmx"}
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
@@ -28,4 +31,3 @@ game.resources = [
 	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
 	 */
 ];
- 
