8c8c8e1334c2568f2f79941ac416438046d4c60f
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-02-04 20:10:25 -0800

    added enemy and player entity base finished video 11

--- js/resources.js
+++ js/resources.js
@@ -7,6 +7,8 @@ game.resources = [
       {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
       {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
       {name: "player", type:"image", src: "data/img/knight.png"},
+      {name: "tower", type:"image", src: "data/img/tower_roung.svg.png"},	
+
 
 
 
